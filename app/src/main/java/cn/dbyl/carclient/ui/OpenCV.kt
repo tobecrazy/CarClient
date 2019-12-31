54b3ae74fd429369146d3a8ba727312c4f04a7e2
Young <young.liu@sap.com>
2019-12-31 21:19:07 +0800

    add new activiy

---
+++ app/src/main/java/cn/dbyl/carclient/ui/OpenCV.kt
@@ -0,0 +1,13 @@
+package cn.dbyl.carclient.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.dbyl.carclient.R
+
+class OpenCV : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_cv)
+    }
+}
