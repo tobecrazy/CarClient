5e56346ad9e8fa592af69a7ad429e05706cab6b6
Young Liu <young.liu@sap.com>
2020-01-31 22:05:33 +0800

    refactor blueTooth initial code

--- app/src/main/java/cn/dbyl/carclient/ClientApp.kt
+++ app/src/main/java/cn/dbyl/carclient/ClientApp.kt
@@ -5,21 +5,17 @@ import android.util.Log
 import cn.dbyl.carclient.database.DataManager
 import cn.dbyl.carclient.database.MyObjectBox
 import cn.dbyl.carclient.utils.NetWorkUtils
-import com.clj.fastble.BleManager
-import com.clj.fastble.scan.BleScanRuleConfig
 import io.objectbox.BoxStore
 import io.objectbox.android.AndroidObjectBrowser
 
 
 /**
- * Create by i321533 (young.liu@sap.com) on 11/24/2019
+ * Create by Young on 11/24/2019
  **/
 class ClientApp : Application() {
      lateinit var boxStore: BoxStore
     override fun onCreate() {
         super.onCreate()
-        //initial blue tooth
-        initialBlueTooth()
         initialGetCurrentIp()
         boxStore=getObjectBox()
         startDatabaseBrowser()
@@ -46,26 +42,7 @@ class ClientApp : Application() {
         return ip
     }
 
-    private fun initialBlueTooth() {
-        BleManager.getInstance().init(this)
-        if (isSupportBlueTooth()) {
-            BleManager.getInstance()
-                .enableLog(true)
-                .setReConnectCount(1, 5000)
-                .setSplitWriteNum(20)
-                .setConnectOverTime(10000).operateTimeout = 5000
-
-            val scanRuleConfig = BleScanRuleConfig.Builder()
-                .setScanTimeOut(10000)
-                .build()
-            BleManager.getInstance().initScanRule(scanRuleConfig)
-        }
-    }
-
     companion object {
         val TAG = ClientApp::class.java.canonicalName
-        fun isSupportBlueTooth(): Boolean {
-            return BleManager.getInstance().isSupportBle
-        }
     }
 }
--- app/src/main/java/cn/dbyl/carclient/ui/BlueToothSettingsActivity.kt
+++ app/src/main/java/cn/dbyl/carclient/ui/BlueToothSettingsActivity.kt
@@ -1,34 +1,86 @@
 package cn.dbyl.carclient.ui
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
 import cn.dbyl.carclient.R
+import com.clj.fastble.BleManager
+import com.clj.fastble.scan.BleScanRuleConfig
+import kotlinx.android.synthetic.main.blue_tooth_settings_activity.*
+import com.clj.fastble.callback.BleScanCallback
+import com.clj.fastble.data.BleDevice
+
 
 class BlueToothSettingsActivity : AppCompatActivity() {
 
+    private val isAutoConnect=true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.settings, SettingsFragment())
-            .commit()
+        setContentView(R.layout.blue_tooth_settings_activity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        initialBlueTooth()
+        initialView()
+    }
+
+    private fun initialView() {
+        blue_tooth_scan.setOnClickListener {
+            BleManager.getInstance().cancelScan()
+            BleManager.getInstance().scan(object:BleScanCallback(){
+                override fun onScanFinished(scanResultList: MutableList<BleDevice>?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onScanStarted(success: Boolean) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onScanning(bleDevice: BleDevice?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+            })
+
+              }
+    }
+
+    private fun initialBlueTooth() {
+        BleManager.getInstance().init(application)
+        if (BleManager.getInstance().isSupportBle) {
+            BleManager.getInstance()
+                .enableLog(true)
+                .setReConnectCount(1, 5000)
+                .setSplitWriteNum(20)
+                .setConnectOverTime(10000).operateTimeout = 5000
+
+            val scanRuleConfig = BleScanRuleConfig.Builder()
+                .setScanTimeOut(10000)
+                .setAutoConnect(isAutoConnect)
+                .build()
+            BleManager.getInstance().initScanRule(scanRuleConfig)
+            if (!BleManager.getInstance().isBlueEnable)
+            {
+                try {
+                    BleManager.getInstance().enableBluetooth()
+                }catch (e:Exception)
+                {
+                    val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    startActivityForResult(intent, 0x01)
+                }
+            }
+        }else
+        {
+//            throw ExceptionInInitializerError("Error, no blue tooth devices")
+        }
+
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId==android.R.id.home)
-        {
+        if (item.itemId == android.R.id.home) {
             finish()
             return true
         }
         return super.onOptionsItemSelected(item)
-    }
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        }
     }
 }
